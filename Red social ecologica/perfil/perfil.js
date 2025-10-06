45e1d8022388e289d3554bb429d65afab9cee25f
GibFerYu <luisfertruma7977@gmail.com>
2025-10-06 10:13:36 -0500

    Add files via upload

---
+++ Red social ecologica/perfil/perfil.js
@@ -0,0 +1,35 @@
+
+
+//Mostrar edicion de perfil
+function mostrarFormulario() {
+  document.getElementById("formEditar").style.display = "block";
+}
+
+//Carga de perfil
+async function cargarPerfil() {
+  const res = await fetch(`/api/perfil`);
+  const data = await res.json();
+
+  document.querySelector(".nombreUS").innerText = data.nombre_usuario;
+  document.querySelector(".perfil").src = data.foto_perfil || "FOTOS/2.jpg";
+  document.getElementById("bio").innerText = data.biografia || "Sin descripción";
+  document.getElementById("nombreUsuario").innerText = data.nombre_usuario;
+  document.getElementById("bioInput").value = data.biografia;
+}
+
+// Guardar cambios
+document.getElementById("formPerfil").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const res = await fetch(`/api/perfil/editar`, {
+    method: "POST",
+    body: formData,
+  });
+
+  const data = await res.json();
+  alert(data.mensaje);
+  cargarPerfil(); 
+});
+
+cargarPerfil();
