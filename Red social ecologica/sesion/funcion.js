2cc4bda9a038f3c94985b3b92d8598f78a46dc4c
GibFerYu <luisfertruma7977@gmail.com>
2025-10-07 00:42:56 -0500

    Update funcion.js

--- Red social ecologica/sesion/funcion.js
+++ Red social ecologica/sesion/funcion.js
@@ -61,7 +61,8 @@ document.addEventListener("DOMContentLoaded", () =>{
   const botonRegistro = document.getElementById('registro');
   botonRegistro.addEventListener('click', (e) =>{
     e.preventDefault();
-    window.location.href = "../registrar/registrar.html";
+    window.location.href = "registrar.html";
   })
 });
+
 
