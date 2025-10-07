98a3aee9435fae6a2180a0c197fa0cbdbdeb56a4
GibFerYu <luisfertruma7977@gmail.com>
2025-10-07 08:16:39 -0500

    Update server.js

--- Red social ecologica/server.js
+++ Red social ecologica/server.js
@@ -55,13 +55,14 @@ app.get("/registrar", (req, res) => {
 });
 
 app.get("/inicio", (req, res) => {
-  res.sendFile(path.join(__dirname, 'Red social ecologica', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
 
 app.listen (PORT, () => {
   console.log(`La vida corre en: http://localhost:${PORT}`);
 });
+
 
 
 
