f494911cd341cebfe993006b1ef473199f3ac30b
GibFerYu <luisfertruma7977@gmail.com>
2025-10-07 11:07:48 -0500

    Update server.js

--- Red social ecologica/server.js
+++ Red social ecologica/server.js
@@ -19,7 +19,7 @@ app.use(session({
 
 //Archivos de rutas estaticas
 
-app.use(express.static(path.join(__dirname, '..')));
+
 app.use(express.static(path.join(__dirname, 'sesion')));
 app.use(express.static(path.join(__dirname, 'registrar')));
 app.use(express.static(path.join(__dirname, 'inicio')));
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'perfil')));
 app.use(express.static(path.join(__dirname, 'retos')));
 app.use(express.static(path.join(__dirname, 'subidas')));
 
+app.use(express.static(path.join(__dirname, '..')));
 //Rutas importantes
 const rutaRegistro = require('./RUTAS/registro');
 const rutaInicio = require('./RUTAS/sesion');
@@ -64,6 +65,7 @@ app.get("/inicio", (req, res) => {
 app.listen (PORT, () => {
   console.log(`La vida corre en: http://localhost:${PORT}`);
 });
+
 
 
 
